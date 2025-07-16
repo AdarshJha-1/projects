e673a7488eee481abe6f2ca9df37236c3520d39e
Adarsh Jha <adarsh2005j@gmail.com>
2025-07-16 19:55:18 +0530

    which simple version

--- which/which.go
+++ which/which.go
@@ -3,14 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 
 
 func main() {
 	args := os.Args[1:]
-	fmt.Println(args)
 
 	pathENV := os.Getenv("PATH")
-	fmt.Println(pathENV)
+	pathArr := strings.Split(pathENV, ":")
+	// fmt.Println(pathArr)
+
+	var argsPath []string
+
+	argLen := len(args)
+	for i := 0; i < argLen; i++ {
+		for _, path := range pathArr {
+			fullPath := path + "/" + args[i]
+			if _, err := os.Stat(fullPath); err == nil {
+				argsPath = append(argsPath, fullPath)
+				break
+			}
+		}
+	}
+
+	for _, path := range argsPath {
+		fmt.Println(path)
+	}
 }
